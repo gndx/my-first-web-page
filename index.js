27c039eded35564da705be2dd5a5298b4a31908a
Oscar Barajas <oscar@dragons.mx>
2019-05-28 22:25:10 -0500

    why is everything broken

---
+++ index.js
@@ -0,0 +1,23 @@
+const API = 'http://api.tvmaze.com/shows';
+const element = document.getElementById('tvshows');
+
+function getData(urlApi) {
+  fetch(urlApi)
+    .then((response) => response.json())
+    .then(function (data) {
+      let output = data.slice(0, 16).map(function (show) {
+        return `
+          <article class="show-card">
+            <img src="${show.image.medium}" alt="Imagen">
+            <h2>${show.name}</h2>
+          </article>
+        `
+      }).join('');
+      element.innerHTML = output;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+getData(API);
